a538c8136cb83644dbbf46fc204f4b7e9019c59c
Pawani Imalsha <imalshapawani95@gmail.com>
2020-06-01 13:52:02 +0530

    add validation to login

--- view/index.php
+++ view/index.php
@@ -23,11 +23,11 @@
                     </div> <!--end of txtUserName-->
 
                     <div class="my-3">  <!--txtpassword-->
-                    <input type="password" id="txtUserName" name="txtUserName" class="form-control" placeholder="Password">
+                    <input type="password" id="txtPassword" name="txtUserName" class="form-control" placeholder="Password">
                     </div> <!--end of txtpassword-->
 
                     <div class="my-3">  <!--button -->
-                        <button class="btn btn-danger btn-block text-uppercase">Log in</button>
+                        <button id="btnLogin" class="btn btn-danger btn-block text-uppercase">Log in</button>
                     </div> <!-- end of button -->
 
                     <hr class="my-4">
@@ -43,6 +43,43 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous">
+</script>
+
+<script>
+
+//btnLogin
+$('#btnLogin').click(
+
+function(){
+    inputValidation();  
+}
+
+); 
+//end btnLogin click event
+
+//input validation
+function inputValidation(callback){
+    //txtUserName validation
+    if ($('#txtUserName').val().length==0){
+        alert('User name can not be empty');
+        return false;
+
+    } //end txtUserName validation
+
+    //txtPassword validation
+    if ($('#txtPassword').val().length==0){
+        alert('Password can not be empty');
+        return false;
+
+    } //end of txtPassword validation
+}
+//end input validation
+
+</script>
 </body>
 
 </html>
